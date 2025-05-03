a2442a47cbc924a698b769eb9d624f468c94b536
Deniz Acay <me@denizacay.com>
2025-05-03 13:23:59 +0300

    Complete project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.arge.CarSkeleton;
+import org.example.arge.ElectricCar;
+import org.example.company.Car;
+import org.example.company.Ford;
+import org.example.company.Holden;
+import org.example.company.Mitsubishi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/main/java/org/example/arge/ElectricCar.java
@@ -0,0 +1,29 @@
+package org.example.arge;
+
+public class ElectricCar extends CarSkeleton {
+
+    private double avgKmPerCharge;
+    private int batterySize;
+
+    public ElectricCar(String name, String description, double avgKmPerCharge, int batterySize) {
+        super(name, description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    public double getAvgKmPerCharge() {
+        return avgKmPerCharge;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void startEngine() {
+    }
+}
